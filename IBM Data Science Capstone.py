b2cd7bf3d1573d0d05f3ef46a43b651b5b968b86
Roseline-T <62439285+Roseline-T@users.noreply.github.com>
2020-03-21 23:34:57 -0300

    Applied Data Science Capstone

---
+++ IBM Data Science Capstone.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # IBM DATA SCIENCE CAPSTONE PROJECT
+# 
+# This Notebook will us be used mainly for the capstone project
+
+# In[5]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[7]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
